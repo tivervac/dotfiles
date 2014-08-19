aefeaf6df0cdf3a22689801e220a15a9be97c542
Titouan Vervack <tivervac@gmail.com>
2014-08-19 17:56:59 +0200

    Added dirsize

--- install.sh
+++ install.sh
@@ -3,11 +3,17 @@ echo "Setting permissions..."
 chmod -R 700 *
 
 echo "Copying files..."
-cp -R .* ~
-rm -rf ~/.git
+cp -R .scripts ~
+cp -R .zsh ~
+
+echo "Creating links..."
+ln -s .gitconfig ~
+ln -s .vimrc ~
+ln -s .zshrc ~
 
 echo "Setting up vim environment..."
 mkdir ~/.vim
 mkdir ~/.vim/tmp
 mkdir ~/.vim/tmp/swap
 mkdir ~/.vim/tmp/backup
+vim +BundleInstall! +qall
