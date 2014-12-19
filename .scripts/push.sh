2a797fb41a73c0dd3c49c08982970e61ee31154e
Titouan Vervack <Titouan.Vervack@UGent.be>
2014-12-19 12:56:53 +0100

    Changes to reflect server changes

--- .scripts/push.sh
+++ .scripts/push.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Thanks to noctua
+set -e
 
 fles="WEBSITE"
 file="$(mktemp XXXXXX.png)"
 scrot $* "$file"
 chmod 755 $file
-scp -p $file flesje:~/Pictures/
+scp -p $file flesje:~/images/
 rm "$file"
-url="$fles":8080/"$file"
+url="$fles":8080/~USER/images/"$file"
 notify-send "Screenshot uploaded to $url"
 echo "$url" | xclip -sel c
