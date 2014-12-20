3e76bbdfbb41b02f1e772aa48936f34cff2cc1e2
Titouan Vervack <tivervac@gmail.com>
2014-12-20 13:33:34 +0100

    Update push.sh

--- .scripts/push.sh
+++ .scripts/push.sh
@@ -2,12 +2,12 @@
 # Thanks to noctua
 set -e
 
-fles="WEBSITE"
+fles="http://flashyoshi.me"
 file="$(mktemp XXXXXX.png)"
 scrot $* "$file"
 chmod 755 $file
 scp -p $file flesje:~/images/
 rm "$file"
-url="$fles":8080/~USER/images/"$file"
+url="$fles":8080/~flashyoshi/images/"$file"
 notify-send "Screenshot uploaded to $url"
 echo "$url" | xclip -sel c
