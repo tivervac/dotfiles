7b3f5acd0ea3379f79b6c6c935768f3d546dbe42
Titouan Vervack <Titouan.Vervack@UGent.be>
2014-12-22 12:43:57 +0100

    100% variable defined push.sh

--- .scripts/push.sh
+++ .scripts/push.sh
@@ -2,12 +2,14 @@
 # Thanks to noctua
 set -e
 
-fles="WEBSITE"
+sshuser="flesje"
+fles="http://flashyoshi.me"
+user=$(cat ~/.ssh/config | sed 's/^ \+//' | sed ':a;N;$!ba;s/\(.\)\n/\1 /g' | grep $sshuser | sed 's/.*User \([^ ]*\) .*/\1/')
 file="$(mktemp XXXXXX.png)"
 scrot $* "$file"
 chmod 755 $file
-scp -p $file flesje:~/images/
+scp -p $file $sshuser:~/images/
 rm "$file"
-url="$fles":8080/~USER/images/"$file"
+url="$fles":8080/~"$user"/images/"$file"
 notify-send "Screenshot uploaded to $url"
 echo "$url" | xclip -sel c
