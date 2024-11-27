c8a89a37a75dceadaf9084d607f923ade01164c6
Titouan Vervack <titouan.vervack@sigasi.com>
2024-11-27 10:13:36 +0100

    Add quartus

--- .local/bin/sigasi-init.sh
+++ .local/bin/sigasi-init.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-export PATH=$PATH:$HOME/.m2/smtools-linux-x64/
+export PATH="$PATH":"$HOME"/.m2/smtools-linux-x64/
 export SM_HOST="https://clientauth.one.digicert.com"
 export SM_CLIENT_CERT_FILE="$HOME/.m2/Sigasi_EV_Code_Signing_Certificate.p12"
+export QSYS_ROOTDIR="/home/t21/intelFPGA_lite/23.1std/quartus/sopc_builder/bin"
 
-alias sgs="cd $HOME/sigasi-dev/git/sigasi"
-alias sgdos="cd $HOME/sigasi-dos/git/sigasi"
-alias sgtres="cd $HOME/sigasi-tres/git/sigasi"
+alias sgs='cd $HOME/sigasi-dev/git/sigasi'
+alias sgdos='cd $HOME/sigasi-dos/git/sigasi'
+alias sgtres='cd $HOME/sigasi-tres/git/sigasi'
 alias dockerlogin="aws ecr get-login-password | docker login --username AWS --password-stdin 906120494814.dkr.ecr.eu-west-1.amazonaws.com"
